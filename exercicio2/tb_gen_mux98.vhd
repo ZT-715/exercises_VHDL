f61dc3291349e0a12479ce3f4dedbc1a9edae457
Gabriel S.Zanella <gabes.zanella@gmail.com>
2024-04-20 08:07:30 -0300

    Finish testbench for gen_mux

--- exercicio2/tb_gen_mux98.vhd
+++ exercicio2/tb_gen_mux98.vhd
@@ -2,21 +2,22 @@ library ieee;
 use ieee.std_logic_1164.all;
 use work.mux_input_array.all;
 
--- 54700ns to run given all std_logic values are tested
+-- 2880ns to run given all std_logic values are tested
 
 entity tb_mux_gen is
 
-    constant CLOCK_PERIOD : time := 20 ns;
-    constant MUX_INPUTS: integer := 2;
-    constant MUX_BUS_SIZE: integer := 8;
+generic(
+    CLOCK_PERIOD : time := 20 ns;
+    MUX_INPUTS: integer := 2;
+    MUX_BUS_SIZE: integer := 8
+);
 
 end entity;
 
 architecture tb of tb_mux_gen is
     signal clk: std_logic := '1';
 
-    signal i: input_arr(0 to MUX_INPUTS-1, MUX_BUS_SIZE-1 downto 0) :=
-    (others => (others => '0'));
+    signal i: input_arr(0 to MUX_INPUTS-1, MUX_BUS_SIZE-1 downto 0) := (others => (others => '0'));
     signal sel: integer range 0 to MUX_INPUTS-1 := 0;
     signal y: output_arr(MUX_BUS_SIZE-1 downto 0);
 
@@ -29,7 +30,6 @@ begin
     clk <= not clk after CLOCK_PERIOD/2;
 
     TB: process
-        variable out_bit, inp_bit: integer range 0 to 1 := 0;
     begin
         for n in integer range 0 to MUX_INPUTS-1 loop
             for b in integer range 0 to MUX_BUS_SIZE-1 loop
@@ -40,19 +40,9 @@ begin
 
                     wait for CLOCK_PERIOD;
 
-                    if(o = '0') then 
-                        inp_bit := 0;
-                    else inp_bit := 1;
-                    end if;
-
-                    if(y(b) = '0') then
-                        out_bit := 0;
-                    else out_bit := 1;
-                    end if;
-
                     assert y(b) = o
-                    report "Output bit " & integer'image(out_bit) & " diverges from input "
-                    & integer'image(n) & " which is " & integer'image(inp_bit)
+                    report "Output bit " & std_logic'image(y(b)) & " diverges from input "
+                    & integer'image(n) & " which is " & std_logic'image(o)
                     severity failure;
                 end loop;
                 
